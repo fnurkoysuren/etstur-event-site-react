7ca84eeba43967b34e574b2ab58c8a877c81021a
Fatma Nur Koysuren <fa.kysrn@gmail.com>
2022-06-19 13:59:23 +0300

    router config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import SiteHeader from './layout/SiteHeader';
 import SiteFooter from './layout/SiteFooter';
+import { Routes, Route } from 'react-router-dom';
+import Events from './pages/Events';
 
 function App() {
 
   return (<>
     <SiteHeader></SiteHeader>
+    <Routes>
+      <Route path={"/"} element={<Events />}></Route>
+    </Routes>
     <SiteFooter></SiteFooter>
   </>);
 }
